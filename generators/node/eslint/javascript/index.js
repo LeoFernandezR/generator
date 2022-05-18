4cf9f053907bde02fbb1e5a35103d5811975ba13
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-05-17 21:15:26 -0300

    fix: remove unnecessary deps

--- generators/node/eslint/javascript/index.js
+++ generators/node/eslint/javascript/index.js
@@ -11,19 +11,12 @@ module.exports = class extends Generator {
         "eslint-plugin-import",
         "eslint-plugin-node",
         "eslint-plugin-prettier",
-        "@typescript-eslint/parser",
-        "@typescript-eslint/eslint-plugin",
-        "typescript",
-        "ts-node-dev",
-        "@types/node",
-        "@tsconfig/node16"
       ],
-      { "save-dev": true }
+      {"save-dev": true}
     );
   }
 
   eslint() {
     this.fs.copy(this.templatePath(".eslintrc"), this.destinationPath(".eslintrc"));
   }
-
 };
